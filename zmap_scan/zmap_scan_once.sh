a0de80a658f4b0fb03a4e6e6a06dafb7f4377495
zy12574 <zhangyang12574@163.com>
2023-04-10 20:30:51 +0800

    zmap scan list of IPv6 addresses ok

--- zmap_scan/zmap_scan_once.sh
+++ zmap_scan/zmap_scan_once.sh
@@ -1,3 +1,3 @@
 # python3 gen_ip_list.py 2001:1201:10::/48 2048 > ips_to_scan.txt
-sudo zmap -M icmp6_echoscan --ipv6-source-ip=2402:f000:2:801:4dd0:4ec9:e35b:e38a -B 100K -G F8:89:D2:80:78:D1 --output-module=csv -o "output.csv" --ipv6-target-file=ips_to_scan.txt 
-sudo zmap -M icmp6_echoscan --ipv6-source-ip=2402:f000:2:801:4dd0:4ec9:e35b:e38a -B 100K -G F8:89:D2:80:78:D1 --output-module=csv -o "output.csv" --ipv6-target-file=ips_to_scan.txt 
+sudo zmap -M ipv6_tcp_synopt -p "80" --ipv6-source-ip=2402:f000:4:1001:809:ba4f:f163:1925 -B 100M --max-runtime=3600 --output-module=csv -o "output.csv" --ipv6-target-file=responsive-addresses.txt
+# sudo zmap -M icmp6_echoscan --ipv6-source-ip=2402:f000:4:1001:809:ba4f:f163:1925 -B 100M -G fc:34:97:e2:07:52 --output-module=csv -o "output.csv" --ipv6-target-file=responsive-addresses.txt
